25e4b743e2f900e4fffcd84f86ebe8146ea99bd9
youbaidjis <youba.etud@gmail.com>
2022-07-28 15:28:19 -0700

    commit tree 1a950f641a246a021faf52aa535d2eb401475316

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_reader :a , :b, :c
+
+  def initialize( a, b, c)
+    if a > 0 && b > 0 && c >0
+    @a = a 
+    @b = b
+    @c = c
+    else 
+      raise TriangleError
+    end
+  end 
+
+  def kind
+    if @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
+      raise TriangleError
+    else
+       if @a == @b && @a == @c
+         :equilateral
+       elsif @a == @b || @a == @c || @b == @c
+         :isosceles
+       else
+         :scalene
+       end
+     end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
